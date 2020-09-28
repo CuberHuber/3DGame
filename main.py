20a0f6fbbd3fc8a71dfe8c31a0a7e0f8b37d0bb2
CuberHuber <romal5754@gmail.com>
2020-09-28 18:23:30 +0500

    Update functions

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import math
-
 import pygame
+
 from settings import *
 from player import Player
 from map import world_map
-import math
 from ray_casting import ray_casting
+from drawing import Drawing
 
 
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+sc_map = pygame.Surface((WIDTH // MAP_SCALE, HEIGHT // MAP_SCALE))
 clock = pygame.time.Clock()
 player = Player()
+drawing = Drawing(screen, sc_map)
 
 
 while True:
@@ -23,16 +25,10 @@ while True:
     player.movement()
     screen.fill(BlACK)
 
-    pygame.draw.rect(screen, SKY, (0, 0, WIDTH, HALF_HEIGHT))
-    pygame.draw.rect(screen, SAND, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
-
-    ray_casting(screen=screen, player_pos=player.pos, player_angle=player.angle)
-
-    # pygame.draw.circle(screen, GREEN, (int(player.x), int(player.y)), 12)
-    # pygame.draw.line(screen, GREEN, player.pos, (player.x + WIDTH * math.cos(player.angle),
-    #                                              player.y + WIDTH * math.sin(player.angle)))
-    # for x,y in world_map:
-    #     pygame.draw.rect(screen, DARKGRAY, (x, y, TILE, TILE), 2)
+    drawing.background()
+    drawing.world(player_pos, player_angle)
+    drawing.fps(clock)
+    drawing.mini_map(player)
 
     pygame.display.flip()
     clock.tick(FPS)
